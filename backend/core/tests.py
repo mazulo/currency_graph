0d467278454b94bc6cefa7c85348adcf451f7280
Patrick Mazulo <pmazulo@gmail.com>
2016-11-30 19:32:36 -0200

    Implement CurrentCurrencyCreate to cover currency creation

--- backend/core/tests.py
+++ backend/core/tests.py
@@ -1,5 +1,12 @@
+from django.conf import settings
 from django.core.urlresolvers import reverse_lazy as r
+from django.utils import timezone
 from django.test import TestCase
+
+import requests
+import requests_mock
+
+from backend.core.models import Currency
 
 
 class IndexTest(TestCase):
@@ -25,3 +32,48 @@ class IndexTest(TestCase):
         for expected in contents:
             with self.subTest():
                 self.assertContains(self.response, expected)
+
+
+class CurrentCurrencyCreate(TestCase):
+
+    def setUp(self):
+
+        self.current_date = timezone.datetime.now()
+        self.base = 'USD'
+        self.symbol_target = 'BRL'
+
+        session = requests.Session()
+        adapter = requests_mock.Adapter()
+        response = {
+            'base': 'USD',
+            'date': '2016-11-29',
+            'rates': {
+                'BRL': 3.4048
+            }
+        }
+
+        URL = settings.API_URL.format(
+            base=self.base,
+            date=self.current_date.isoformat()
+        )
+
+        session.mount('http://api.fixer.io/', adapter)
+
+        adapter.register_uri('GET', URL, json=response, status_code=200)
+
+        self.resp = session.get(URL)
+
+    def test_api_status_code(self):
+        self.assertEqual(self.resp.status_code, 200)
+
+    def test_create_currency(self):
+        json_response = self.resp.json()
+
+        Currency.objects.create(
+            date=self.current_date,
+            base=self.base,
+            symbol_target=self.symbol_target,
+            value=json_response['rates']['BRL']
+        )
+
+        self.assertTrue(Currency.objects.exists())
