a12dd7d05a0fd963d458b4612bc7de638de44f47
Patrick Mazulo <pmazulo@gmail.com>
2016-11-30 19:31:25 -0200

    Implement IndexTest to cover index page

--- backend/core/tests.py
+++ backend/core/tests.py
@@ -1,3 +1,27 @@
+from django.core.urlresolvers import reverse_lazy as r
 from django.test import TestCase
 
-# Create your tests here.
+
+class IndexTest(TestCase):
+
+    def setUp(self):
+        self.response = self.client.get(r('core:index'))
+
+    def test_get(self):
+        """GET / must return status code"""
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+        """Must use index.html"""
+        self.assertTemplateUsed(self.response, 'core/index.html')
+
+    def test_speakers(self):
+        """Must show keynote speakers"""
+        contents = [
+            'Currency graphs',
+            'Dollar',
+            'Euro',
+        ]
+        for expected in contents:
+            with self.subTest():
+                self.assertContains(self.response, expected)
