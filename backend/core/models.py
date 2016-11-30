a89642588a9ce4748e57d22be4f9383592013670
Patrick Mazulo <pmazulo@gmail.com>
2016-11-30 01:39:55 -0200

    Create Currency model and migration file

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 
 class CreateUpdateModel(models.Model):
@@ -14,3 +15,27 @@ class CreateUpdateModel(models.Model):
 
     class Meta:
         abstract = True
+
+
+class Currency(CreateUpdateModel):
+
+    date = models.DateField(verbose_name=_('currency date'))
+    base = models.CharField(
+        verbose_name=_('currency base'),
+        max_length=3)
+    symbol_target = models.CharField(
+        verbose_name=_('currency target'),
+        max_length=3
+    )
+    value = models.DecimalField(
+        verbose_name=_('currency value'),
+        decimal_places=2,
+        max_digits=10,
+    )
+
+    def __str__(self):
+        return '{base} -> {symbol_target}: {value}'.format(
+            base=self.base,
+            symbol_target=self.symbol_target,
+            value=self.value
+        )
